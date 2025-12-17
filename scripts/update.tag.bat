6e97f29a6b7a367ccd7604908f2ef9a74c3eb38c
TooMindoo <tomas.minac@dataquest.sk>
2025-12-17 09:55:35 +0100

    Update tag

--- scripts/update.tag.bat
+++ scripts/update.tag.bat
@@ -1,6 +1,6 @@
-git push origin :te-dep-3.9
-git tag -d te-dep-3.9
-git tag te-dep-3.9
-git push origin te_base_dep_3.9 --tags
+git push origin :te-dep-3.12_new_paddle
+git tag -d te-dep-3.12_new_paddle
+git tag te-dep-3.12_new_paddle
+git push origin te_base_dep_3.12_new_paddle --tags
 
 pause
