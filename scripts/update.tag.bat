1388858f59201a18aeff64cbc7e2454b50da76a4
jm <jm@maz>
2026-01-13 14:35:29 +0100

    paddle 3.3

--- scripts/update.tag.bat
+++ scripts/update.tag.bat
@@ -1,6 +1,6 @@
-git push origin :te-dep-3.12-paddle3
-git tag -d te-dep-3.12-paddle3
-git tag te-dep-3.12-paddle3
-git push origin te_base_dep_3.12_paddle_3 --tags
+git push origin :te-dep-3.12-paddle33
+git tag -d te-dep-3.12-paddle33
+git tag te-dep-3.12-paddle33
+git push origin te_base_dep_3.12_paddle_33 --tags
 
 pause
